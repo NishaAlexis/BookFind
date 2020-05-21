eb4e4a0e5d28e1b683b7b668db10ee6afce29deb
NishaAlexis <alexispadura@gmail.com>
2020-05-22 04:58:49 +0800

    fixed login and register

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const cookieParser = require("cookie-parser");
 const MongoDBStore = require('connect-mongodb-session')(session);
-
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json())
@@ -27,9 +27,12 @@ const store = new MongoDBStore({
 });
 const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  console.log('Connected to Database')
-  const db = client.db('bookFind')
-  const bookCollection = db.collection('book')
+  console.log('Connected to Database');
+  const db = client.db('bookFind');
+  const bookCollection = db.collection('book');
+  const userCollection = db.collection('user');
+  const profileCollection = db.collection('account');
+
 
   // ========================
   // Middlewares
@@ -39,32 +42,89 @@ client.connect(err => {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json())
   app.use(express.static('public'));
+  // app.use(cookieParser());
 
-  // app.use(session({
-  //   secret: 'work hard',
-  //   resave: true,
-  //   saveUninitialized: true,
-  //   store: store
+  // app.use( session({
+  //   key: 'user_sid',
+  //   secret: "book recommender app",
+  //   resave: false,
+  //   saveUninitialized: false,
+  //   store: store,
+  //   cookie: {
+  //     expires: 600000
+  //   } 
   // }));
 
-  // var login = require('./api/login');
-  // app.use('/', login);
+  var User = "";
 
+  app.get('/', function(req, res){
+    res.render('register', {
+      title: "Create an Account",
+      breadcrumb: "Register",
+      page: "Register"
+    });
+  })
+  app.post('/', function(req, res){
+    User = req.body.username;
+    userCollection.insertOne({
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password
+    })
+    res.redirect('/account');
+  })
+
+  app.get('/login', (req, res) => {
+    res.render('login', {
+      title: "Sign In to Account",
+      breadcrumb: "Login",
+      page: "Login"
+    });
+  })
+  app.post('/login', (req, res) => {
+    userCollection.findOne({username : req.body.username}, function(err, user) {
+      if (user === null){
+        res.redirect('/login');
+      } else if (user.username === req.body.username && user.password === req.body.password) {
+        User = req.body.username;
+        res.redirect('/account');
+      } else {
+        console.log("Credentials Wrong");
+        res.redirect('/login');
+      }
+    })
+  })
+
+  function capital_letter(str) {
+    str = str.split(" ");
+    for (var i = 0, x = str.length; i < x; i++) {
+        str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+    }
+    return str.join(" ");
+  }
+
+  var bookSearch = '';
   app.get('/home', function(req, res){
     bookCollection.find({}).limit(12).toArray()
       .then(book => {
         res.render('index', {
           title: "Home",
-          header: "Welcome",
+          header: "Welcome " + User,
           book: book
         });
       })
       .catch(err)
   });
+  app.post('/home', function(req, res){
+    console.log(req.body.search);
+    var query = req.body.search;
+    bookSearch = query;
+    res.redirect('/browse');
+  })
 
   app.get('/book', (req, res) => {
     bookCollection.aggregate([
-      { $match : { id: 1000004 }},
+      { $match : { title: "To Kill a Mockingbird" }},
       { $lookup: {
         from: 'recommendations',
         localField: 'id',
@@ -82,7 +142,7 @@ client.connect(err => {
         // console.log(book[0].recoms_info[0].image_url)
         res.render('single-book', {
           title: "Book Details",
-          header: "Welcome",
+          header: "Welcome " + User,
           breadcrumb: "Book Details",
           page: "Single Book",
           book: book
@@ -90,76 +150,123 @@ client.connect(err => {
       })
       .catch(err)
   })
+  app.get('/book/:id', function(req, res){
+    console.log(req.params.id);
+    res.end();
+  })
+
+  app.get('/account', function(req, res){
+    if (User != "") {
+      res.render('account', {
+        title: "My Account",
+        header: "Welcome " + User,
+        breadcrumb: "My Account",
+        page: "Account"
+      });
+    } else {
+      res.redirect('/');
+    }
+  });
+  app.post('/account', function(req, res) {
+    profileCollection.insertOne({
+      email: req.body.email,
+      fullname: req.body.fullname,
+      gender: req.body.gender,
+      age_range: req.body.age,
+      genre1: req.body.genre1, 
+      genre2: req.body.genre2,
+      genre3: req.body.genre3
+    })
+    res.redirect('/browse')
+  })
 
   app.get('/contact', function(req, res){
     res.render('contact', {
       title: "Contact Us",
-      header: "Welcome",
+      header: "Welcome " + User,
       breadcrumb: "Contact Us",
       page: "Contact"
     });
   });
   
-  app.get('/account', function(req, res){
-    res.render('account', {
-      title: "My Account",
-      header: "Welcome",
-      breadcrumb: "My Account",
-      page: "Account"
-    });
-  });
-  
+  // app.post('/contact', function(req, res){
+  //   var name = req.body.username;
+  //   var email =req.body.email; 
+  //   var phone = req.body.phone; 
+  //   var message = req.body.message; 
+
+  //   var data = { 
+  //     "name": name, 
+  //     "email":email, 
+  //     "phone":phone,
+  //     "message": message
+  //   } 
+  //   contactCollection.insertOne(data, function(err, collection){
+  //     if (err) throw err; 
+  //       console.log("Record inserted Successfully");      
+  //   }); 
+  //   console.log('Data received:\n' + JSON.stringify(req.body))
+  //   return res.redirect('/home'); 
+  // })
+
+  var genreSearch = '';
   app.get('/browse', function(req, res){
-    bookCollection.find({}).limit(12).toArray()
+    //TODO: add if statement for account holders
+    if (bookSearch != '') {
+      bookCollection.find({$text: {$search: bookSearch}}).limit(12).toArray()
       .then(book => {
         res.render('featured', {
           title: "Featured Books",
+          header: "Welcome " + User,
           breadcrumb: "Featured Books",
           page: "Featured",
           book: book
         });
       })
       .catch(err)
+    } else if (genreSearch != '') {
+      bookCollection.aggregate([
+        { $match : { $text: {$search: genreSearch}}},
+        { $sample: {size: 12}}
+      ]).toArray()
+      .then(book => {
+        res.render('featured', {
+          title: "Featured Books",
+          header: "Welcome " + User,
+          breadcrumb: "Featured Books",
+          page: "Featured",
+          book: book
+        });
+      })
+      .catch(err)
+    } else {
+      bookCollection.find({}).limit(12).toArray()
+      .then(book => {
+        res.render('featured', {
+          title: "Featured Books",
+          header: "Welcome " + User,
+          breadcrumb: "Featured Books",
+          page: "Featured",
+          book: book
+        });
+      })
+      .catch(err)
+    }
+    
   });
-
-  // app.get('/browse/fantasy', function(req, res){
-  //   bookCollection.find({content: {$regex: "Fantasy"}}).limit(9).toArray()
-  //     .then(book => {
-  //       res.render('youngadult', {
-  //         title: "Featured Books | Young Adult",
-  //         breadcrumb: "Featured Books | Young Adult",
-  //         page: "Featured",
-  //         book: book
-  //       });
-  //     })
-  //     .catch(err)
-  // });
-
-  // app.get('/browse/fantasy', function(req, res){
-  //   bookCollection.find({content: {$regex: "Fantasy"}}).limit(9).toArray()
-  //     .then(book => {
-  //       res.render('featured', {
-  //         title: "Featured Books",
-  //         breadcrumb: "Featured Books",
-  //         page: "Featured",
-  //         book: book
-  //       });
-  //     })
-  //     .catch(err)
-  // });
-
-  // app.get('/browse/fantasy', function(req, res){
-  //   bookCollection.find({content: {$regex: "Fantasy"}}).limit(9).toArray()
-  //     .then(book => {
-  //       res.render('featured', {
-  //         title: "Featured Books",
-  //         breadcrumb: "Featured Books",
-  //         page: "Featured",
-  //         book: book
-  //       });
-  //     })
-  //     .catch(err)
-  // });
+  app.get('/browse/:genre', function(req, res){
+    var genre = req.params.genre;
+    if (genre === "youngadult"){
+      genreSearch = "young adult";
+    } else if (genre === "sciencefiction") {
+      genreSearch = "science fiction";
+    } else if (genre === "classics") {
+      genreSearch = genre;
+    } else if (genre === "fantasy") {
+      genreSearch = genre;
+    }
+    res.redirect('/browse')
+  })
   
   // app.get('/wishlist', function(req, res){
   //   res.render('wishlist', {
